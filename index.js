8c31b38fb59c2ce1f0279e694207c2ceac3e2d96
Gerardo Flores <30551778+gerardofloresdev@users.noreply.github.com>
2022-06-19 12:55:39 -0600

    Change

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 import Torogoz from './src/core/torogoz.js';
 import TypeDocuments from './src/interfaces/typeDocuments.js';
 
-const deparments = Torogoz.getMunicipalities('01');
-
-console.log(deparments);
-
 export {
     Torogoz,
     TypeDocuments
